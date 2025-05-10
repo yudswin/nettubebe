445e30069565266df0584129a1448c2d22cbdeec
Duy Nguyễn <93112625+yudswin@users.noreply.github.com>
2025-05-10 17:09:58 +0700

    cors disable

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,19 @@ import bodyParser from 'body-parser';
 import dotenv from "dotenv";
 import { getDBClient } from "@db/client";
 import { routes } from "@routes/index";
-import cors from 'cors'
-import { getCorsOptions } from '@libs/corsSetting';
+// import cors from 'cors'
+// import { getCorsOptions } from '@libs/corsSetting';
 
 dotenv.config()
 
 const PORT = process.env.PORT || 3000;
 const app = express()
-if (process.env.CORS_ENABLE === 'production') {
-    app.use(cors(getCorsOptions()));
-}
+
+// if (process.env.CORS_ENABLE === 'production') {
+//     app.use(cors(getCorsOptions()));
+//     console.log('enabled')
+// }
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(bodyParser.json());
