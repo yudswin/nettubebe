973210033a68f01ee4ca43339a36bdc60d9eb66b
Duy Nguyễn <93112625+yudswin@users.noreply.github.com>
2025-05-10 16:07:36 +0700

    cors

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,9 @@ dotenv.config()
 
 const PORT = process.env.PORT || 3000;
 const app = express()
-app.use(cors(getCorsOptions()));
-
+if (process.env.CORS_ENABLE === 'production') {
+    app.use(cors(getCorsOptions()));
+}
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(bodyParser.json());
