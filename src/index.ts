bf6d38306a2994bc286a4a9d46f1598d1044a834
Duy Nguyễn <93112625+yudswin@users.noreply.github.com>
2025-05-20 03:53:09 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,14 @@ import dotenv from "dotenv";
 import { getDBClient } from "@db/client";
 import { routes } from "@routes/index";
 import cors from 'cors'
-import { getCorsOptions } from '@libs/corsSetting';
+// import { getCorsOptions } from '@libs/corsSetting';
 
 dotenv.config()
 
 const PORT = process.env.PORT || 3000;
 const app = express()
 
-app.use(cors(getCorsOptions))
+app.use(cors())
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
